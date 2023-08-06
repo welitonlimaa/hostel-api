49806468f98cd4a6ae777de1b2b0cb031a771659
welitonlimaa <welitonlima159@gmail.com>
2023-08-06 15:25:29 -0300

    feat: reserva dto e repository

---
+++ src/main/java/com/example/hostelapi/dto/ReservaDTO.java
@@ -0,0 +1,46 @@
+package com.example.hostelapi.dto;
+
+import com.example.hostelapi.domain.Reserva;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class ReservaDTO implements Serializable {
+
+  private Long id;
+  private String nomeHospede;
+
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  private LocalDate dataInicio;
+
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  private LocalDate dataFim;
+
+  private int quantidadePessoas;
+  private String status;
+
+  public ReservaDTO(Reserva obj) {
+    id = obj.getId();
+    nomeHospede = obj.getNomeHospede();
+    dataInicio = obj.getDataInicio();
+    dataFim = obj.getDataFim();
+    quantidadePessoas = obj.getQuantidadePessoas();
+    status = obj.getStatus();
+  }
+
+  public ReservaDTO(Long id, String nomeHospede, LocalDate dataInicio, LocalDate dataFim, int quantidadePessoas, String status) {
+    this.id = id;
+    this.nomeHospede = nomeHospede;
+    this.dataInicio = dataInicio;
+    this.dataFim = dataFim;
+    this.quantidadePessoas = quantidadePessoas;
+    this.status = status;
+  }
+}
---
+++ src/main/java/com/example/hostelapi/repository/ReservaRepository.java
@@ -0,0 +1,9 @@
+package com.example.hostelapi.repository;
+
+import com.example.hostelapi.domain.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+}
