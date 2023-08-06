f159d711316c5c00aef79dbb3baba3d8f4c81d34
welitonlimaa <welitonlima159@gmail.com>
2023-08-06 14:42:02 -0300

    first commit

---
+++ src/test/java/com/example/hostelapi/HostelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hostelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
